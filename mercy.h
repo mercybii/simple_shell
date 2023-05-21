2280adbf334d5eefc3b08732814cbec0641e8cb5
mercybii <mercybii95@gmail.com>
2023-05-21 22:15:31 +0300

    all

--- mercy.h
+++ mercy.h
@@ -2,7 +2,6 @@
 #define MERCY_H
 
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -22,10 +21,6 @@
 #define B_SIZ 1024
 
 
-
-#include "data_handling.h"
-#include "function.h"
-#include "simple_shell.h"
 
 #define CD_HELP_INFO
 
